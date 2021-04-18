1f6de0c573101ea6c34ddf7ba9f111831438ecce
Cabeda <jecabeda@gmail.com>
2021-04-18 16:47:14 +0100

    Add default merge global

--- setups/git-setup.sh
+++ setups/git-setup.sh
@@ -4,3 +4,5 @@ git config --global user.name "José Cabeda"
 git config --global user.email "jecabeda@gmail.com"
 git config --global core.pager "delta --line-numbers --dark"
 git config --global delta.side-by-side true
+
+git config --global pull-rebase false
