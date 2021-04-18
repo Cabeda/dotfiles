b61278eff55c95cbe457b1f51bbc5b5af1051cde
Cabeda <jecabeda@gmail.com>
2021-04-18 16:46:05 +0100

    Move setup files to folder. Add git config file

---
+++ setups/git-setup.sh
@@ -0,0 +1,6 @@
+
+
+git config --global user.name "José Cabeda"
+git config --global user.email "jecabeda@gmail.com"
+git config --global core.pager "delta --line-numbers --dark"
+git config --global delta.side-by-side true
--- setups/termux-setup.sh
+++ setups/termux-setup.sh
@@ -12,7 +12,13 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 
 
 # Set dotfiles configs to be the ones used by the system
-ln -s .tmux.conf ~/.tmux.conf
-ln -s .vimrc ~/.vimrc
-ln -s .zshrc ~/.zshrc
-ln -s .alacritty.yml ~/.alacritty.yml
+# ln -s .tmux.conf ~/.tmux.conf
+# ln -s .vimrc ~/.vimrc
+# ln -s .zshrc ~/.zshrc
+# ln -s .alacritty.yml ~/.alacritty.yml
+
+
+# Git setup ssh key
+eval $(ssh-agent)
+ssh-add
+ssh-add ~/.ssh/id_rsa
