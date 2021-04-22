de91b9fa6bd068be2a998b81ac2a673e30f0cde7
Cabeda <jecabeda@gmail.com>
2021-04-22 18:25:41 +0100

    Add openssh and add z-zsh

--- setups/termux-setup.sh
+++ setups/termux-setup.sh
@@ -2,14 +2,13 @@
 
 # A script to set up termux
 
-apt install git entr zsh
-
-# Setup git
+apt install git entr zsh openssh
 
 
 # Install ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
 
 # Set dotfiles configs to be the ones used by the system
 # ln -s .tmux.conf ~/.tmux.conf
