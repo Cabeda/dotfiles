528e554ffbd5bdba4d5df01ad0903d7183319b6a
Cabeda <jecabeda@gmail.com>
2021-05-15 18:32:02 +0100

    Auto commit

--- write.sh
+++ write.sh
@@ -1,10 +1,13 @@
 #bin/bash
 
+WIKI_DIR="~/Documents/Git/pensamentos/"
+
 if [ ${1-"mac"} == "windows" ] 
 then
     echo "Starting windows"
 else
-    open -a typora ~/Documents/Git/pensamentos/
-    ~/Documents/Git/pensamentos/git_sync.sh
+    cd $WIKI_DIR
+    open -a typora .
+    git_sync.sh
     open -a spotify
 fi
