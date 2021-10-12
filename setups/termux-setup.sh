0a462f50e9fdc52086ce2edcf424cffe2049a90d
Cabeda <jecabeda@gmail.com>
2021-10-12 19:58:30 +0100

    Reorder files

--- setups/termux-setup.sh
+++ setups/termux-setup.sh
@@ -5,21 +5,19 @@
 # Enable storage 
 termux-setup-storage
 
-# Install packages
-pkg install git zoxide bat
-
-apt install entr zsh openssh
-
-
-# Install oh my ZSH
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 # Set dotfiles configs to be the ones used by the system
 ln -s .tmux.conf ~/.tmux.conf
 ln -s .vimrc ~/.vimrc
 ln -s .zshrc ~/.zshrc
 ln -s .alacritty.yml ~/.alacritty.yml
 
+# Install packages
+pkg install git zoxide bat
+
+apt install entr zsh openssh
+
+# Install oh my ZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Git setup ssh key
 export SSH_AUTH_SOCK=$HOME/.sshagent
