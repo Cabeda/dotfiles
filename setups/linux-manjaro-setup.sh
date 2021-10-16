f3dc726e8e4d8894de52d458359e5c9b0c866e03
José cabeda <Cabeda>
2021-10-16 11:00:09 +0100

    Add new programs to linux

--- setups/linux-manjaro-setup.sh
+++ setups/linux-manjaro-setup.sh
@@ -17,7 +17,8 @@ sudo pacman -Sy
 sudo pacman -S alacritty bitwarden tldr git \
                 fzf z vlc github-cli docker \
                 pwgen jq yq nodejs postgresql \
-                ripgrep steam vim tmux nodejs gnome-keyring
+                ripgrep steam vim tmux nodejs gnome-keyring \
+                zoxide flameshot
 
 curl https://zoom.us/client/latest/zoom_x86_64.pkg.tar.xz --output ~/Downloads/zoom.pkg.tar.xz
 sudo pacman -U ~/Downloads/zoom.pkg.tar.xz
