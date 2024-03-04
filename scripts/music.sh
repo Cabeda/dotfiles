b8e9b9ca573a297a4ca2f9a15fc3d2c3a154ef6f
José Cabeda <jecabeda@gmail.com>
2024-03-04 17:53:20 +0000

    Update scripts

---
+++ scripts/music.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title music
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🎧
+# @raycast.argument1 {"type": "dropdown", "placeholder": "type", "data": [{"title": "Synth", "value": "https://www.youtube.com/live/4xDzrJKXOOY"}, {"title": "Lofi", "value": "https://www.youtube.com/live/jfKfPfyJRdk"}, {"title": "offline", "value": "/Users/jose.cabeda/Git/dotfiles/synth.mp4"}] }
+
+# Documentation:
+# @raycast.author José cabeda
+
+
+
+mpv $1 --loop
