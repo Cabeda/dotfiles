f5f5fbce21e90dc7d7c4a950fa455f85118c8626
José Cabeda <jecabeda@gmail.com>
2024-03-30 16:26:41 +0000

    Add notes when available

--- scripts/omnivore.ts
+++ scripts/omnivore.ts
@@ -1,26 +1,29 @@
 import { parseArgs } from "https://deno.land/std@0.207.0/cli/parse_args.ts";
 interface Article {
-    id: string;
-    title: string;
-    url: string;
-    author?: string;
-    wordsCount: number;
-  }
-  
-  interface Edge {
-    node: Article;
-  }
-  
-  interface Response {
-    data: {
-      search: {
-        edges: Edge[];
-      };
+  id: string;
+  title: string;
+  url: string;
+  author?: string;
+  wordsCount: number;
+  highlights: {
+    annotation: string;
+  }[];
+}
+
+interface Edge {
+  node: Article;
+}
+
+interface Response {
+  data: {
+    search: {
+      edges: Edge[];
     };
-  }
-  
-  async function retrieveNewsletter(): Promise<Article[] | undefined> {
-    const graphql = `query Search($after: String, $first: Int, $query: String) {
+  };
+}
+
+async function retrieveNewsletter(): Promise<Article[] | undefined> {
+  const graphql = `query Search($after: String, $first: Int, $query: String) {
       search(first: $first, after: $after, query: $query) {
         ... on SearchSuccess {
           edges {
@@ -30,6 +33,9 @@ interface Article {
               url
               author
               wordsCount
+              highlights {
+                annotation
+              }
             }
           }
         }
@@ -39,65 +45,67 @@ interface Article {
       }
     }
       `;
-  
-    if (!API_KEY) {
-      console.error("Missing OMNIVORE_API_KEY");
-      Deno.exit(1);
-    }
-  
-    try {
-      const response = await fetch("https://api-prod.omnivore.app/api/graphql", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          authorization: API_KEY,
+
+  if (!API_KEY) {
+    console.error("Missing OMNIVORE_API_KEY");
+    Deno.exit(1);
+  }
+
+  try {
+    const response = await fetch("https://api-prod.omnivore.app/api/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: API_KEY,
+      },
+      body: JSON.stringify({
+        query: graphql,
+        variables: {
+          query: "in:inbox label:newsletter",
+          first: 100,
         },
-        body: JSON.stringify({
-          query: graphql,
-          variables: {
-            query: "in:inbox label:newsletter",
-            first: 100,
-          },
-        }),
-      });
-  
-      const json: Response = await response.json();
-  
-      return json.data.search.edges.map((x) => x.node) as Article[];
-    } catch (error) {
-      console.error(error.message);
-      throw new Error(error);
-    }
+      }),
+    });
+
+    const json: Response = await response.json();
+
+    return json.data.search.edges.map((x) => x.node) as Article[];
+  } catch (error) {
+    console.error(error.message);
+    throw new Error(error);
   }
-  
-  function convertToMarkdown(articles: Article[]): string {
-    const markdown = articles
-      .map((article) => {
-        const author = article.author ? ` by ${article.author}` : "";
-        return `- [${article.title}${author}](${article.url})`;
-      })
-      .join("\n");
-  
-    return markdown;
+}
+
+function convertToMarkdown(articles: Article[]): string {
+  const markdown = articles
+    .map((article) => {
+      const author = article.author ? ` by ${article.author}` : "";
+      return `- [${article.title}${author}](${article.url}) ${
+        article.highlights ? ": " + article.highlights[0]?.annotation : ""
+      }`;
+    })
+    .join("\n");
+
+  return markdown;
+}
+
+async function archiveNewsletters(articles: Article[]): Promise<void> {
+  console.log("Archiving articles with newsletter label");
+
+  try {
+    await articles.map((article) => {
+      archiveNewsletter(article.id);
+    });
+  } catch (error) {
+    console.error(error.message);
+    throw new Error(error);
   }
-  
-  async function archiveNewsletters(articles: Article[]): Promise<void> {
-    console.log("Archiving articles with newsletter label");
-  
-    try {
-      await articles.map((article) => {
-        archiveNewsletter(article.id);
-      });
-    } catch (error) {
-      console.error(error.message);
-      throw new Error(error);
-    }
-    console.log("Articles archived");
-  }
-  
-  async function getTotalArticles(query = "in:inbox no:label") {
-    try {
-      const graphql = `query Search($after: String, $first: Int, $query: String) {
+  console.log("Articles archived");
+}
+
+async function getTotalArticles(query = "in:inbox no:label") {
+  try {
+    const graphql = `query Search($after: String, $first: Int, $query: String) {
         search(first: $first, after: $after, query: $query) {
           ... on SearchSuccess {
             pageInfo {
@@ -110,36 +118,36 @@ interface Article {
         }
       }    
       `;
-  
-      if (!API_KEY) {
-        console.error("Missing OMNIVORE_API_KEY");
-        Deno.exit(1);
-      }
-  
-      const result = await fetch("https://api-prod.omnivore.app/api/graphql", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          authorization: API_KEY,
-        },
-        body: JSON.stringify({
-          query: graphql,
-          variables: {
-            query,
-          },
-        }),
-      });
-  
-      return await result.json();
-    } catch (error) {
-      console.error(error.message);
-      throw new Error(error);
+
+    if (!API_KEY) {
+      console.error("Missing OMNIVORE_API_KEY");
+      Deno.exit(1);
     }
+
+    const result = await fetch("https://api-prod.omnivore.app/api/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: API_KEY,
+      },
+      body: JSON.stringify({
+        query: graphql,
+        variables: {
+          query,
+        },
+      }),
+    });
+
+    return await result.json();
+  } catch (error) {
+    console.error(error.message);
+    throw new Error(error);
   }
-  
-  async function archiveNewsletter(id: string): Promise<void> {
-    try {
-      const graphql = ` mutation SetLinkArchived($input: ArchiveLinkInput!) {
+}
+
+async function archiveNewsletter(id: string): Promise<void> {
+  try {
+    const graphql = ` mutation SetLinkArchived($input: ArchiveLinkInput!) {
           setLinkArchived(input: $input) {
             ... on ArchiveLinkSuccess {
               linkId
@@ -152,65 +160,62 @@ interface Article {
           }
         }
       `;
-  
-      if (!API_KEY) {
-        console.error("Missing OMNIVORE_API_KEY");
-        Deno.exit(1);
-      }
-  
-      const result = await fetch("https://api-prod.omnivore.app/api/graphql", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          authorization: API_KEY,
-        },
-        body: JSON.stringify({
-          query: graphql,
-          variables: {
-            input: {
-              archived: true,
-              linkId: id,
-            },
-          },
-        }),
-      });
-  
-      await result.json();
-    } catch (error) {
-      console.error(error.message);
-      throw new Error(error);
+
+    if (!API_KEY) {
+      console.error("Missing OMNIVORE_API_KEY");
+      Deno.exit(1);
     }
-  }
-  
-  const API_KEY = Deno.env.get("OMNIVORE_API_KEY");
-  
-  if (!API_KEY) {
-    console.error("Missing OMNIVORE_API_KEY");
-    Deno.exit(1);
-  }
-  const flags = parseArgs(Deno.args, {
-    boolean: ["total", "archive"],
-    string: ["query"],
-  });
 
+    const result = await fetch("https://api-prod.omnivore.app/api/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: API_KEY,
+      },
+      body: JSON.stringify({
+        query: graphql,
+        variables: {
+          input: {
+            archived: true,
+            linkId: id,
+          },
+        },
+      }),
+    });
 
-  
-  if (flags.total) {
-    if (flags.query) console.log(flags.query);
-    const total = await getTotalArticles(flags.query);
-    console.log(`${total.data.search.pageInfo.totalCount} articles found`);
-    Deno.exit(0);
+    await result.json();
+  } catch (error) {
+    console.error(error.message);
+    throw new Error(error);
   }
-  
-  const articles = await retrieveNewsletter();
-  
-  if (!articles) {
-    console.error("No articles found");
-    Deno.exit(1);
-  }
-  console.log(await convertToMarkdown(articles));
-  
-  if (flags.archive) {
-    archiveNewsletters(articles);
-  }
-  
+}
+
+const API_KEY = Deno.env.get("OMNIVORE_API_KEY");
+
+if (!API_KEY) {
+  console.error("Missing OMNIVORE_API_KEY");
+  Deno.exit(1);
+}
+const flags = parseArgs(Deno.args, {
+  boolean: ["total", "archive"],
+  string: ["query"],
+});
+
+if (flags.total) {
+  if (flags.query) console.log(flags.query);
+  const total = await getTotalArticles(flags.query);
+  console.log(`${total.data.search.pageInfo.totalCount} articles found`);
+  Deno.exit(0);
+}
+
+const articles = await retrieveNewsletter();
+
+if (!articles) {
+  console.error("No articles found");
+  Deno.exit(1);
+}
+console.log(await convertToMarkdown(articles));
+
+if (flags.archive) {
+  archiveNewsletters(articles);
+}
