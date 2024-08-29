53d54b4a533fba9398ba193b7dc6cebab952f647
José Cabeda <jecabeda@gmail.com>
2024-08-29 10:08:10 +0100

    update start

--- scripts/start.sh
+++ scripts/start.sh
@@ -24,7 +24,6 @@ else
     open -a "Google Chrome" 
     open -a "Microsoft Outlook"
     open -a "Microsoft Teams"
-    open -a "Firefox" https://www.music.youtube.com
     open -a "Firefox" https://www.gocomics.com/random/calvinandhobbes
     open -a "bitwarden"
     open -a "Microsoft To Do"
