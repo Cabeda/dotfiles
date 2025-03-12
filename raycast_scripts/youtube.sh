699cbb39dd565e81a8b0d41d597fe3e52cbc8093
José Cabeda <jecabeda@gmail.com>
2025-03-12 13:31:04 +0000

    Refactor Raycast scripts: migrate start.py and start.sh to raycast_scripts directory and remove old versions

---
+++ raycast_scripts/youtube.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Youtube
+# @raycast.mode fullOutput
+#
+# Optional parameters:
+# @raycast.icon 📹
+# @raycast.packageName Youtube
+#
+# Documentation:
+# @raycast.description Youtube open
+# @raycast.author José Cabeda
+# @raycast.authorURL https://github.com/cabeda/dotfiles
+
+open "https://www.youtube.com"
