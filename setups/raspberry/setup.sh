61115b3e182dd7d5c6304d5e447ec38329f6ed5e
José Cabeda <cabeda@proton.me>
2025-11-05 10:52:25 +0000

    Add rust, hx and bluetui

--- setups/raspberry/setup.sh
+++ setups/raspberry/setup.sh
@@ -4,10 +4,13 @@
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
 sudo apt install jellyfin transmission \
-    tealdeer fzf ripgrep zsh zoxide trash-cli yt-dlp
+    tealdeer fzf ripgrep zsh zoxide trash-cli yt-dlp \
+    hx speedtest
 
 curl -sS https://starship.rs/install.sh | sh
 curl -LSfs https://raw.githubusercontent.com/cantino/mcfly/master/ci/install.sh | sh -s -- --git cantino/mcfly
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh 
+sudo apt install libdbus-1-dev pkg-config && cargo install bluetui
 
 touch /root/.zsh_history
 eval "$(mcfly init zsh)"
