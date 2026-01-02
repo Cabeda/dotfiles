35cd92e865c950dfbc5548931cb808da9ef4b696
José Cabeda <jecabeda@gmail.com>
2026-01-02 12:03:22 +0000

    fix: update Jira link in start.py for new board

--- scripts/start.py
+++ scripts/start.py
@@ -52,7 +52,7 @@ def main():
 
         links = [
             "https://gitlab.cicd.man/dashboard/merge_requests"
-            "https://jira.collaboration-man.com/secure/RapidBoard.jspa?rapidView=2470&projectKey=MANAI&quickFilter=12286#"
+            "https://jira.collaboration-man.com/secure/RapidBoard.jspa?rapidView=3578&projectKey=MANAI&view=detail&selectedIssue=MANAI-933#"
             "https://mail.google.com",
             "https://mail.proton.me"
         ]
